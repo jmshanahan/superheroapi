e08c3302ac6b8665f81133caae0e4963dac65506
jmshanahan <comeraghsolutions@gmail.com>
2023-01-17 11:26:43 +1100

    Parsed data to only return what was needed and fixed route with id

--- routes/index.mjs
+++ routes/index.mjs
@@ -1,43 +1,66 @@
 import { default as express, json } from "express";
-import axios from 'axios';
+import axios from "axios";
 export const router = express.Router();
 
-import DBG from 'debug';
-const debug = DBG('server:router-root');
-const error = DBG('server:error-root');
+import DBG from "debug";
+const debug = DBG("server:router-root");
+const error = DBG("server:error-root");
 
 const todo = {
   id: 1,
   task: "Get the milk",
 };
 
-const getSuperHerors = async () => {
+const getSuperHerors = async (id) => {
   try {
     const TOKEN = process.env.SUPER_HERO_TOKEN;
     const response = await axios.get(
-      `https://superheroapi.com/api/${TOKEN}/1`,{"Content-Type`": "application/json"}
+      `https://superheroapi.com/api/${TOKEN}/${id}`,
+      { "Content-Type`": "application/json" }
     );
-    // if (!response.ok) {
-    //   throw new Error("Something went wrong!");
-    // }
 
-    // const data = await response.json();
-    debug(`The response id is ${response.data.id}`);
-    debug(`The response powerstats ${response.data.powerstats.strength}`);
+    const { powerstats, image } = response.data;
+    const responseData = {
+      id: response.data.id,
+      response: response.data.response,
+      name: response.data.name,
+      powerstats: {
+        intelligence: powerstats.intelligence,
+        strength: powerstats.strength,
+        speed: powerstats.speed,
+        durability: powerstats.durability,
+        power: powerstats.power,
+        combat: powerstats.combat,
+      },
+      image: {
+        url: image.url,
+      },
+    };
 
-
-    return response.data;
+    return responseData;
   } catch (error) {
-    return (error.message);
+    return error.message;
   }
 };
 
 /* GET home page. */
 router.get("/", async (req, res, next) => {
   try {
-    const data = await getSuperHerors();
+    const id = 1;
+
+    const data = await getSuperHerors(id);
     res.send(data);
-  
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    // debug(`The req  is ${req.params.id}`);
+
+    const data = await getSuperHerors(req.params.id);
+    res.send(data);
   } catch (err) {
     next(err);
   }
--- server.mjs
+++ server.mjs
@@ -1,43 +1,52 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import { default as express } from 'express';
-import { default as bodyParser } from 'body-parser';
-import { approotdir } from './approotdir.mjs';
-import * as http from 'http';
-import cors from 'cors';
-import { default as logger } from 'morgan';
-import { default as rfs } from 'rotating-file-stream';
+import { default as express } from "express";
+import { default as bodyParser } from "body-parser";
+import { approotdir } from "./approotdir.mjs";
+import * as http from "http";
+import cors from "cors";
+import { default as logger } from "morgan";
+import { default as rfs } from "rotating-file-stream";
 
-import { default as DBG } from 'debug';
-const debug = DBG('server:debug'); 
-const dbgerror = DBG('server:error'); 
+import { default as DBG } from "debug";
+const debug = DBG("server:debug");
+const dbgerror = DBG("server:error");
 
 import {
-    normalizePort, onError, onListening, handle404, basicErrorHandler
-} from './appsupport.mjs';
-
+  normalizePort,
+  onError,
+  onListening,
+  handle404,
+  basicErrorHandler,
+} from "./appsupport.mjs";
 
 // server.listen(port, () => console.log(`Strted on port ${port}`));
-import { router as indexRouter } from './routes/index.mjs';
+import { router as indexRouter } from "./routes/index.mjs";
 
 const __dirname = approotdir;
 const app = express();
 
-export const port = normalizePort(process.env.PORT || '3000');
+export const port = normalizePort(process.env.PORT || "3000");
 
-app.use(logger(process.env.REQUEST_LOG_FORMAT || 'combined', {
+app.use(
+  logger(process.env.REQUEST_LOG_FORMAT || "combined", {
     // immediate: true,
-    stream: process.env.REQUEST_LOG_FILE ?
-        rfs.createStream(process.env.REQUEST_LOG_FILE, {
-            size:     '10M', // rotate every 10 MegaBytes written
-            interval: '1d',  // rotate daily
-            compress: 'gzip' // compress rotated files
+    stream: process.env.REQUEST_LOG_FILE
+      ? rfs.createStream(process.env.REQUEST_LOG_FILE, {
+          size: "10M", // rotate every 10 MegaBytes written
+          interval: "1d", // rotate daily
+          compress: "gzip", // compress rotated files
         })
-        : process.stdout
-}));
+      : rfs.createStream("superhero.log", {
+          size: "10M", // rotate every 10 MegaBytes written
+          interval: "1d", // rotate daily
+          compress: "gzip", // compress rotated files
+        }),
+    //process.stdout
+  })
+);
 
-
-app.set('port', port);
+app.set("port", port);
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -47,18 +56,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(handle404);
 app.use(basicErrorHandler);
 
-
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 export const server = http.createServer(app);
 
 server.listen(port);
-server.on('request', (req, res) => {
-    debug(`${new Date().toISOString()} request ${req.method} ${req.url}`);
+server.on("request", (req, res) => {
+  debug(`${new Date().toISOString()} request ${req.method} ${req.url}`);
 });
-server.on('error', onError);
-server.on('listening', onListening);
-
+server.on("error", onError);
+server.on("listening", onListening);
 
 export default app;
-
